5ccec5e17e8513fa022bd88f494c1e3a1ea3ea76
xiang <91250402+marinesea99@users.noreply.github.com>
2022-05-20 10:21:05 +0800

    Update week12_5_web_pde.js

--- week12_5_web_pde/week12_5_web_pde.js
+++ week12_5_web_pde/week12_5_web_pde.js
@@ -16,14 +16,20 @@ function draw(){
         rect(50, 100, 150, 2);
     if (choice == 2)
         rect(50, 150, 150, 2);
+    if (choice == 3)
+        rect(50, 200, 150, 2);
+    if (choice == 4)
+        rect(50, 250, 150, 2);
     text("中餐吃泡麵", 50, 50);
     text("中餐吃UberEat", 50, 100);
     text("中餐吃FoodPanda", 50, 150);
+    text("中餐吃牛排", 50, 200);
+    text("中餐吃大餐", 50, 250);
 }
 
 function mousePressed() {
     // choice=(choice + 1)%3;
-    choice = int(random(3));
+    choice = int(random(5));
 }
 
 function initializeFields(){
