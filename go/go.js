76a9766d51b7206230441ad99a1733589100e11d
xiang <91250402+marinesea99@users.noreply.github.com>
2022-06-10 11:57:18 +0800

    add go 五子棋

---
+++ go/go.js
@@ -0,0 +1,41 @@
+function setup() {
+    initializeFields();
+    createCanvas(500, 500);
+}
+// 棋子的座標 陣列
+var bx;
+// 棋子的座標 陣列
+var by;
+// 棋子的數字,一開始0個棋子
+var N;
+function draw() {
+    background(color(0xFF, 0xFF, 0xF2));
+    for (var y = 50; y < 500; y += 50) {
+        line(0, y, 500, y);
+    }
+    for (var x = 50; x < 500; x += 50) {
+        line(x, 0, x, 500);
+    }
+    for (var i = 0; i < N; i++) {
+        if (i % 2 == 0)
+            fill(0);
+        else
+            fill(255);
+        ellipse(bx[i], by[i], 40, 40);
+    }
+    if (N % 2 == 0)
+        fill(0);
+    else
+        fill(255);
+    ellipse(mouseX, mouseY, 40, 40);
+}
+function mousePressed() {
+    bx[N] = mouseX;
+    by[N] = mouseY;
+    N++;
+}
+function initializeFields() {
+    bx = new Array(100);
+    by = new Array(100);
+    N = 0;
+}
